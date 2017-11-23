18355e00b87f707cf6ce23ebcd15d4c57b06cfff
Harrison Balogh <harrisonbalogh@yahoo.com>
2017-11-23 01:55:58 -0800

    Key Generation

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var obj = {
    secret: rand(128, 10)
 };
 var json = JSON.stringify(obj);
-// fs.writeFile('key.json', json, 'utf8', callback);
+fs.writeFile('key.json', json, 'utf8', callback);
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;

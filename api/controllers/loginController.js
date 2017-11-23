80d3d6230cc0f4630f80ff5b3c65a86a18fdfe68
Harrison Balogh <harrisonbalogh@yahoo.com>
2017-11-23 00:25:33 -0800

    Token Implementation

--- api/controllers/loginController.js
+++ api/controllers/loginController.js
@@ -21,14 +21,14 @@ exports.login = function(req, res) {
         const payload = {
           name: user.name
         };
-        // var token = jwt.sign(payload, config.secret, {
-        //   expiresInMinutes: 1440 // expires in 24 hours
-        // });
-        // res.json({
-        //   success: true,
-        //   message: 'Token produced!',
-        //   token: token
-        // });
+        var token = jwt.sign(payload, config.secret, {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+        res.json({
+          success: true,
+          message: 'Token produced!',
+          token: token
+        });
         res.json({ success: true, message: 'Login successful: ' + config.secret});
       }
     }

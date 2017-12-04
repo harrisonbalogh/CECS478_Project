f820d4e1926b872e54bf28914c1ccb67d7442d7d
Harrison Balogh <harrisonbalogh@yahoo.com>
2017-12-04 15:57:23 -0800

    Refactor

--- server.js
+++ server.js
@@ -21,30 +21,31 @@ var obj = {
 var json = JSON.stringify(obj);
 fs.writeFileSync('key.json', json);
 
+messageApp.use(bodyParser.urlencoded({ extended: true }));
+messageApp.use(bodyParser.json());
+
+// =============== Login App Server (8080) ===============
+loginApp.use(bodyParser.urlencoded({ extended: true }));
+loginApp.use(bodyParser.json());
 // Mongoose instance connection url
 mongoose.Promise = global.Promise;
 mongoose.connect(config.message_database);
-
-loginApp.use(bodyParser.urlencoded({ extended: true }));
-loginApp.use(bodyParser.json());
-
+// Routes
 var middleware = require('./api/middleware');
-var messengerRoutes = require('./api/routes/messengerRoutes'); //importing route
-var userRoutes = require('./api/routes/userRoutes'); //importing route
-
-userRoutes(loginApp); //register the routes
-middleware(loginApp); //NOTE: Order that the middleware gets loaded is important.
-// All routes loaded below the middleware must have JWT authentication.
-messengerRoutes(loginApp); //register the routes
-
+var messengerRoutes = require('./api/routes/messengerRoutes');
+var userRoutes = require('./api/routes/userRoutes');
+// Register routes. NOTE: Order that the middleware gets loaded is important.
+userRoutes(loginApp);
+middleware(loginApp); // routes loaded below middleware must have JWT authentication.
+// messengerRoutes(loginApp);
 // Nginx is acting as a reverse proxy with HTTPS setup and redicts from HTTP.
 // Nginx listens locally on the server to port 8080 responding to http://127.0.0.1
 loginApp.listen(port); // http://127.0.0.1:8080
 
+// =============== Websocket Server (10001) ===============
 // Setup socket
 var socketServer = messageApp.listen(10001); // http://127.0.0.1:10001
 var io = require('socket.io')(socketServer).of('/messages');
-
 // io.on('connection', socketioJwt.authorize({
 //     secret: JSON.parse(fs.readFileSync("key.json")).secret,
 //     timeout: 15000 // 15 seconds to send the authentication message
@@ -52,7 +53,6 @@ var io = require('socket.io')(socketServer).of('/messages');
 //     //this socket is authenticated, we are good to handle more events from it.
 //     console.log('hello! ' + socket.decoded_token.name);
 // });
-
 io.on('connection', function (socket) {
   console.log("On connection...");
   socket.emit('message', {content: 'somemessage'});
